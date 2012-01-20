78aef09c867a9ad24aa62effa018c741ef703043
Dominik Pospisil <dpospisi@redhat.com>
2012-01-20 09:46:13 +0000

    Added ManagementOnlyModeTestCase.

---
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/management/api/ManagementOnlyModeTestCase.java
@@ -0,0 +1,99 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.as.test.integration.management.api;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.URL;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.as.controller.client.ModelControllerClient;
+import org.jboss.as.test.integration.management.base.AbstractMgmtTestBase;
+import org.jboss.as.test.integration.management.cli.GlobalOpsTestCase;
+import org.jboss.dmr.ModelNode;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Assert;
+import static org.jboss.as.test.integration.management.util.ModelUtil.createOpNode;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.ADMIN_ONLY;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.SUCCESS;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.OUTCOME;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.READ_ATTRIBUTE_OPERATION;
+import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.NAME;
+import static org.jboss.as.arquillian.container.Authentication.getCallbackHandler;
+
+/**
+ *
+ * @author Dominik Pospisil <dpospisi@redhat.com>
+ */
+@RunWith(Arquillian.class)
+@RunAsClient
+public class ManagementOnlyModeTestCase extends AbstractMgmtTestBase {
+
+    @ArquillianResource
+    URL url;
+    
+    @Deployment
+    public static Archive<?> getDeployment() {
+        JavaArchive ja = ShrinkWrap.create(JavaArchive.class, "dummy.jar");
+        ja.addClass(GlobalOpsTestCase.class);
+        return ja;
+    }
+    
+    @Before
+    public void before() throws IOException {
+        initModelControllerClient(url.getHost(), MGMT_PORT);
+    }
+
+    @AfterClass
+    public static void after() throws IOException {
+        closeModelControllerClient();
+    }    
+
+    @Test
+    public void testManagementOnlyMode() throws Exception {
+        
+        // restart server to management-only mode
+        ModelNode op = createOpNode(null, "reload");        
+        op.get(ADMIN_ONLY).set(true);        
+        
+        try {
+            ModelNode result = executeOperation(op);
+        } catch (Exception e) {}
+        
+        Thread.sleep(20000);
+        
+        // check that the server is admin-only mode
+        ModelControllerClient cl = ModelControllerClient.Factory.create(InetAddress.getByName(url.getHost()), MGMT_PORT, getCallbackHandler());
+        op = createOpNode(null, READ_ATTRIBUTE_OPERATION);        
+        op.get(NAME).set("running-mode");        
+        ModelNode ret = cl.execute(op);                                
+        Assert.assertTrue(SUCCESS.equals(ret.get(OUTCOME).asString()));
+    }
+}
